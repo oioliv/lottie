d3d3a1f7ae223b7b6522083c50b5344b30367a96
Oioliv <135426768+oioliv@users.noreply.github.com>
2023-06-03 11:36:16 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello, test2</h1>
+      <h1>Hello, je suis Olivia</h1>
       <h2>Start editing to see some magic happen!</h2>
       <Example />
     </div>
