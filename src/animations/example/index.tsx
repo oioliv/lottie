a6590370975265db0a4dc5595a1e3d8978d73297
Oioliv <135426768+oioliv@users.noreply.github.com>
2023-06-03 10:52:12 +0000

    Update 6 files

---
+++ src/animations/example/index.tsx
@@ -0,0 +1,8 @@
+import Lottie from "lottie-react";
+import groovyWalkAnimation from "./groovyWalk.json";
+
+const Example = () => (
+  <Lottie animationData={groovyWalkAnimation} loop={true} />
+);
+
+export default Example;
